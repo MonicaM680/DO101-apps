3ff99205f7a67a1fe4dc50a3962f4f7e3aa800b8
MonicaM680 <munirajmonica12@gmail.com>
2024-10-12 15:59:39 -0400

    updated

--- weather/app.js
+++ weather/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 
-var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,6 +19,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+var app = express();
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
